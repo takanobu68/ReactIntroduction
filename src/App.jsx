3aad14b00d1e6ca82c97973b7e1a60b5f1e22a01
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-06-02 00:06:12 +0900

    State useState()の使い方

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfullMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
-
+  const [num, setNum] = useState(0);
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
       <ColorfullMessage color="pink">元気です!</ColorfullMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
